2e9bfd32821e15a8408afbf41aceea6f928c9ca3
Anujeet Swain <anujeet98@gmail.com>
2024-06-28 20:18:20 +0530

    typescript configuration for dev env

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, {Request, Response} from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response)=>{
-    res.send('Testing initial load');
+    res.send('Testing initial load, watch testing');
 })
 
 app.listen(PORT, ()=>{
