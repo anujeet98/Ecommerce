3f3b10fe3193a0f483c1f5d792276dfc2288c3b5
Anujeet Swain <anujeet98@gmail.com>
2024-06-29 07:48:29 +0530

    Created order schema

---
+++ src/models/order.ts
@@ -0,0 +1,53 @@
+import mongoose, { Schema } from "mongoose";
+import { IProduct } from "./product";
+
+
+export interface IOrderItem extends Document {
+    product: IProduct;
+    quantity: number;
+    price: number;
+}
+
+export interface IOrder extends Document {
+    user: string;
+    items: IOrderItem[];
+    totalPrice: number;
+    createdAt: Date;
+}
+
+
+const orderItemSchema = new Schema({
+    product: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Product', 
+        required: true },
+    quantity: { 
+        type: Number, 
+        required: true 
+    },
+    price: { 
+        type: Number, 
+        required: true 
+    }
+})
+
+
+const orderSchema = new Schema({
+    user: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    items: [orderItemSchema],
+    totalPrice: { 
+        type: Number, 
+        required: true 
+    },
+    createdAt: { 
+        type: Date, 
+        required: true, 
+        default: Date.now 
+    }
+});
+
+export default mongoose.model('Order', orderSchema);
